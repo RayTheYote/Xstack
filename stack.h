2fb9c55f8bf8479edb92107c5850b8775560a21f
Psychdelic_Coyote <63522483+RayTheYote@users.noreply.github.com>
2022-08-25 13:04:51 -0300

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,36 @@
+#include <iostream>
+
+namespace xstack{
+	struct nodo{
+		int valor;
+		nodo *siguiente_nodo;
+	};
+	
+	nodo *puntero_maestro=NULL;
+	
+	void push(int val){
+		nodo *nodo_dummy=new nodo;
+		nodo_dummy->valor=val;
+		nodo_dummy->siguiente_nodo=puntero_maestro;
+		puntero_maestro=nodo_dummy;
+	}
+	
+	int pop(){
+		nodo *nodo_dummy=puntero_maestro;
+		puntero_maestro=nodo_dummy->siguiente_nodo;
+		if(delete nodo_dummy){
+			return 0;
+		}
+		else{
+			return -1;
+		}
+	}
+
+	void show(){
+		nodo *nodo_dummy=puntero_maestro;
+		while(nodo_dummy != NULL){
+			std::cout<<nodo_dummy->valor<<"\n";
+			nodo_dummy=nodo_dummy->siguiente_nodo;
+		}
+	}
+}
