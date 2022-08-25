dce99a209586fb0da31edd0bbd243b1f5c610fb4
Psychdelic_Coyote <63522483+RayTheYote@users.noreply.github.com>
2022-08-25 13:21:02 -0300

    Update xstack.h
    Se agregaron comentarios basicos

--- xstack.h
+++ xstack.h
@@ -1,21 +1,21 @@
 #include <iostream>
 
 namespace xstack{
-	struct nodo{
-		int valor;
-		nodo *siguiente_nodo;
+	struct nodo{ //Definicion de la estructura nodo
+		int valor; //Valor que tendra cada nodo
+		nodo *siguiente_nodo; //Apuntadaor encargado de apuntar al valor de memoria del nodo siguiente al que se definio
 	};
 	
-	nodo *puntero_maestro=NULL;
+	nodo *puntero_maestro=NULL; //Puntero encargado de apuntar el nodo en el tope de la pila
 	
-	void push(int val){
+	void push(int val){ //Funcion encargada de agregar nodos a la pila
 		nodo *nodo_dummy=new nodo;
 		nodo_dummy->valor=val;
 		nodo_dummy->siguiente_nodo=puntero_maestro;
 		puntero_maestro=nodo_dummy;
 	}
 	
-	int pop(){
+	int pop(){ //Funcion encargada de quitar el nodo en el tope de la pila
 		nodo *nodo_dummy=puntero_maestro;
 		puntero_maestro=nodo_dummy->siguiente_nodo;
 		if(delete nodo_dummy){
@@ -26,7 +26,7 @@ namespace xstack{
 		}
 	}
 
-	void show(){
+	void show(){ //Funcion encargada de mostrar todos los elementos de la pila
 		nodo *nodo_dummy=puntero_maestro;
 		while(nodo_dummy != NULL){
 			std::cout<<nodo_dummy->valor<<"\n";
@@ -34,11 +34,11 @@ namespace xstack{
 		}
 	}
 	
-	int first_element(){
+	int first_element(){ //Funcion encargada de devolver el primer nodo que se ingreso a la pila
 		return puntero_maestro->valor;
 	}
 	
-	int last_element(){
+	int last_element(){ //Funcion encargada de devolver el ultimo nodo que se ingreso a la pila
 		nodo *nodo_dummy=puntero_maestro;
 		while(nodo_dummy->siguiente_nodo != NULLL){
 			nodo_dummy=nodo_dummy->siguiente_nodo;
